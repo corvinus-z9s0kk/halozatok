3a6ddd66c6bc69f9f243e542e1d9c6906252e491
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-04-15 12:51:26 +0200

    Controller

--- HajosTest/HajosTest/Startup.cs
+++ HajosTest/HajosTest/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,13 +33,18 @@ namespace HajosTest
 
             app.UseRouting();
 
-           /* app.UseEndpoints(endpoints =>
+            /* app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapGet("/", async context =>
+                 {
+                     await context.Response.WriteAsync("Hello World!");
+                 });
+             });*/
+
+            app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });*/
+                endpoints.MapControllers();
+            });
         }
     }
 }
