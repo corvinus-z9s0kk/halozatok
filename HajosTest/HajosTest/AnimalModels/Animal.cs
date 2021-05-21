664c4ee38ebc3501ae658e0918f9840c512a58ac
Gábor Csepei <gabor.csepei@stud.uni-corvinus.hu>
2021-05-21 16:14:06 +0200

    Beadandó

---
+++ HajosTest/HajosTest/AnimalModels/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HajosTest.AnimalModels
+{
+    public partial class Animal
+    {
+        public int Id { get; set; }
+        public string AnimalName { get; set; }
+    }
+}
---
+++ HajosTest/HajosTest/Controllers/AnimalController.cs
@@ -0,0 +1,72 @@
+﻿using HajosTest.AnimalModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HajosTest.Controllers
+{
+    [Route("api/animal")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        // api/animal/count
+        [HttpGet]
+        [Route("count")]
+        public int M1()
+        {
+            AnimalDatabaseContext context = new AnimalDatabaseContext();
+            int állatokSzáma = context.Animals.Count();
+            return állatokSzáma;
+        }
+
+        // GET: api/animal>
+        [HttpGet]
+        public IEnumerable<Animal> Get()
+        {
+            AnimalDatabaseContext context = new AnimalDatabaseContext();
+            return context.Animals.ToList();
+        }
+
+        // GET api/animal/5
+        [HttpGet("{id}")]
+        public Animal Get(int id)
+        {
+            AnimalDatabaseContext context = new AnimalDatabaseContext();
+            var keresettÁllat = (from x in context.Animals
+                                 where x.Id == id
+                                 select x).FirstOrDefault();
+            return keresettÁllat;
+        }
+
+        // POST api/<AnimalController>
+        [HttpPost]
+        public void Post([FromBody] Animal újÁllat)
+        {
+            AnimalDatabaseContext context = new AnimalDatabaseContext();
+            context.Animals.Add(újÁllat);
+            context.SaveChanges();
+        }
+
+        // PUT api/<AnimalController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AnimalController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            AnimalDatabaseContext context = new AnimalDatabaseContext();
+            var törlendőÁllat = (from x in context.Animals
+                                 where x.Id == id
+                                 select x).FirstOrDefault();
+            context.Animals.Remove(törlendőÁllat);
+            context.SaveChanges();
+        }
+    }
+}
